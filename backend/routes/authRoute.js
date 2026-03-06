e45b5b57ebc20f0f311d6cdcd8eff5e6f083e00c
AbhinavNeema <abhinavneema22@gmail.com>
2026-03-07 00:30:35 +0530

    fix sso login

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -12,5 +12,17 @@ authRouter.post("/sendotp",sendOtp)
 authRouter.post("/verifyotp",verifyOtp)
 authRouter.post("/resetpassword",resetPassword)
 
+authRouter.get("/sso-token", (req, res) => {
 
+  const token = req.cookies.token;
+
+  console.log("🔐 Cookie token:", token);
+
+  if (!token) {
+    return res.status(401).json({ error: "Not logged in" });
+  }
+
+  res.json({ token });
+
+});
 export default authRouter
